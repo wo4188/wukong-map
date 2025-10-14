1a436b004ded28e7039fdb190d709ecd762aea5f
wozy <1297981152@qq.com>
2025-10-15 00:45:06 +0800

    安装依赖，导入静态资源

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+//
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>cc You did it!</main>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+//
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,34 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import autoImport from 'unplugin-auto-import/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(), //
     vueDevTools(),
+    autoImport({
+      imports: [
+        'vue', //
+        'pinia',
+        'vue-router',
+      ],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/assets/styles/scss-var.scss" as *;
+          @use "@/assets/styles/mixin.scss" as *;
+        `,
+      },
     },
   },
 });
