a3fa07f36bc2826b22995bab087ed2b8d7b1c513
wozy <1297981152@qq.com>
2025-10-27 02:52:34 +0800

    调整 相关资源路径，测试 部署

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
   <router-view />
 </template>
+
+<script setup lang="ts">
+console.groupCollapsed('🚩 [env] 环境变量');
+console.log(import.meta.env);
+console.groupEnd();
+</script>
--- src/api/tmplApi.ts
+++ src/api/tmplApi.ts
@@ -1,6 +1,8 @@
 import { http } from '@/utils';
 
-const PREFIX = '/templates';
+const base = import.meta.env.BASE_URL;
+
+const PREFIX = `${base}templates`;
 
 export function getMarkerTmpl() {
   return http.get<unknown, string>(`${PREFIX}/marker.ejs`);
